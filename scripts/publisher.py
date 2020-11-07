843760d349ba8f2bc869db9ebb378f2f78d1d7db
larbiom <larbiomari1@gmail.com>
2020-11-07 12:45:12 +0100

    test de nom

--- scripts/publisher.py
+++ scripts/publisher.py
@@ -13,7 +13,7 @@ def talker():
     while not rospy.is_shutdown():
         
         #move_cmd = Twist()
-        move_cmd.linear.x = input("Valeur vitesse (float): ")
+        move_cmd.linear.x = input("Valeur  vitesse (float): ")
         #rospy.loginfo(hello_str)
         pub.publish(move_cmd)
         rate.sleep()
