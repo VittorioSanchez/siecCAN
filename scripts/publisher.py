27a9ed461429d6b2d5c91de5e2199ad396f1bb3b
larbiom <pi@raspberrypi>
2020-11-07 12:34:34 +0100

    ajout de publisher.py

---
+++ scripts/publisher.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+#from std_msgs.msg import String
+from geometry_msgs.msg import Twist 
+
+def talker():
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    move_cmd = Twist() #create a twist message
+    #move_cmd.linear.x = 1.0
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        
+        #move_cmd = Twist()
+        move_cmd.linear.x = input("Valeur vitesse (float): ")
+        #rospy.loginfo(hello_str)
+        pub.publish(move_cmd)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
